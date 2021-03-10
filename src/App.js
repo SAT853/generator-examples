892942f4eb92e20436bb33e6eabf8ede8e7383a7
Sathishkumar Raja <sathishkumar.raja@one.verizon.com>
2021-03-10 23:03:54 +0530

    initial commir for generator function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Generator ====>
+  // Intro What's generator
+  // 1. Generators Syntax
+  // 2. Yield*
+  // 3. Passing arguments into Generator
+  // 4. Passing arguments into Generator 1
+  // 5. Passing Yield as an Argument of a Function 1
+  // 6. Yield with a Function Call
+  // 7. Yield with Promises
+  // 8. Yield* with a Built-in Iterable Object
+  return <div className="App">A Simple Guide to Understanding Javascript (ES6) Generators</div>;
 }
 
 export default App;
